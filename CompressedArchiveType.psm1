933baf650a65557ab6c14f5ec204e26cac04963f
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2022-05-16 21:45:31 -0300

    Adicionando enum de tipos de arquivo comprimidos

---
+++ CompressedArchiveType.psm1
@@ -0,0 +1,11 @@
+enum CompressedArchiveType {
+    _7z; XZ; BZIP2; GZIP;
+    TAR; ZIP; WIM; AR; ARJ;
+    CAB; CHM; CPIO; CramFS;
+    DMG; EXT; FAT; GPT; HFS;
+    IHEX; ISO; LZH; LZMA;
+    MBR; MSI; NSIS; NTFS;
+    QCOW2; RAR; RPM; SquashFS;
+    UDF; UEFI; VDI; VHD; VHDX;
+    VMDK; XAR; Z;
+}
